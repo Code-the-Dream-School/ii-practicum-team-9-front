872a50f33b3eeba1c4a42131b3054c5bb090f850
Filicity SH <f.elena.sh@gmail.com>
2025-04-10 15:31:05 -0700

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-
 import { ToastContainer } from "react-toastify";
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
-import { Home, Register, Login , ForgetPassword ,ResetPassword} from "./pages";
-import { Home, Register, Login} from "./pages";
-import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Home, Register, Login, ForgetPassword, ResetPassword } from "./pages";
+
+import React, { useState } from "react";
 
 import NavBar from "./components/NavBar/NavBar";
 import AddItemModal from "./components/AdditemModal/AddItemModal";
@@ -13,32 +11,26 @@ import Barter from "./pages/Barter";
 import './App.css';
 import './index.css';
 
-const URL = 'http://localhost:8000/api/v1/';
- 
+//const URL = 'http://localhost:8000/api/v1/';
+
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
   return (
-    <BrowserRouter>
-      <ToastContainer/>
-      {!isAuthenticated ?  (
-         <Routes>
-         {/* Redirect all routes to login if not authenticated */}
-         <Route path="*" element={<Navigate to="/login" />} />
-         <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-         <Route path="/register" element={<Register />} />
-         <Route path="/forgetpassword" element={<ForgetPassword />} />
-         <Route path="/resetpassword" element={<ResetPassword />} />
+    <Router>
       <ToastContainer />
       {!isAuthenticated ? (
         <Routes>
+          {/* Redirect all routes to login if not authenticated */}
           <Route path="*" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/forgetpassword" element={<ForgetPassword />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
         </Routes>
       ) : (
         <div className="app-container">
@@ -53,8 +45,8 @@ const App = () => {
           {isModalOpen && <AddItemModal closeModal={closeModal} />}
         </div>
       )}
-    </BrowserRouter>
+    </Router>
   );
 };
 
-export default App
+export default App;
