8714a7852e3eba30d92eda0640aa427f2fdc1287
tidiong <62657965+tidiong@users.noreply.github.com>
2025-05-06 13:08:18 -0600

    Cleaned up error handling

--- src/components/AdditemModal/AddItemModal.jsx
+++ src/components/AdditemModal/AddItemModal.jsx
@@ -54,17 +54,13 @@ const AddItemModal = ({ closeModal }) => {
       closeModal();
     } catch (error) {
       console.error('Error adding item:', error);
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
+      if (error.response) { 
         console.error('Error response data:', error.response.data);
-        console.error('Error response status:', error.response.status);
-        console.error('Error response headers:', error.response.headers);
       } else if (error.request) {
-        // The request was made but no response was received
+         
         console.error('Error request:', error.request);
       } else {
-        // Something happened in setting up the request that triggered an Error
+       
         console.error('Error message:', error.message);
       }
       alert('Failed to add item. Please try again.');
